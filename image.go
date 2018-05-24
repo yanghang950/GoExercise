0fc9d8eec466596d40158d3e45c60b97473c43a2
Hang Yang <36313467+yanghang950@users.noreply.github.com>
2018-05-24 21:30:34 +0800

    Add files via upload

---
+++ image.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"golang.org/x/tour/pic"
+	"image"
+	"image/color"
+)
+
+type Image struct {
+	width, height int
+}
+
+func (i Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, i.width, i.height)
+}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i Image) At(x, y int) color.Color {
+	v := uint8(x * y)
+	return color.RGBA{v, v, 255, 255}
+}
+
+func main() {
+	m := Image{40, 100}
+	pic.ShowImage(m)
+}
+
+
